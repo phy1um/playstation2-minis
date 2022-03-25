dc484229733a4c9934924f971caa7c91ed8069be
Tom Marks <tom@tommarks.xyz>
2022-03-26 10:55:44 +1100

    collision system

--- pew/script/entity/rock.lua
+++ pew/script/entity/rock.lua
@@ -1,12 +1,14 @@
 
 local D2D = require("draw2d")
 local M = require("ps2math")
+local entity = require("entity.entity")
+local AABB = require("aabb")
 
 local CX = 320
 local CY = 224
 local RROT = 0.3
 
-local rock = {
+local rock = entity.define({
   pos = M.vec2(0, 0),
   rv = 1.4,
   theta = 0,
@@ -15,7 +17,7 @@ local rock = {
   scale = 1,
   wrap = true,
   name = "ROCK",
-}
+})
 
 function rock.new(x, y)
   local dc = M.vec2(CX - x, CY - y)
@@ -25,20 +27,35 @@ function rock.new(x, y)
   local out = setmetatable({
     pos = M.vec2(x, y),
     dir = dc,
+    aabb = AABB.new(x, y, 20, 20)
   }, {__index = rock})
-
+  out.aabb.kind = "playerdamage"
   return out
 end
 
+function rock:onSpawn(st)
+  st:addArea(self.aabb)
+end
+
 function rock:update(dt)
+  if self.kill == true then return false end
   local delta = M.vec2From(self.dir)
   delta:scale(self.speed)
   self.pos:add(delta)
+  self.aabb.pos:add(delta)
 end
 
 function rock:draw()
   D2D:setColour(0xff, 0xff, 0, 0x80)
   D2D:rect(self.pos.x, self.pos.y, 20, 20)
+end
+
+function rock:collide(aabb)
+  if aabb.kind == "enemydamage" and aabb:testAABB(self.aabb) then
+    LOG.info("OW")
+    self.kill = true
+    aabb.signal("kill")
+  end
 end
 
 
--- pew/script/game.lua
+++ pew/script/game.lua
@@ -2,14 +2,15 @@
 local D2D = require("draw2d")
 local State = require("state")
 
-local player = require("player")
+local player = require("entity.player")
 local rock = require("entity.rock")
-local gameManager = require("gamemgr")
+local gameManager = require("entity.gamemgr")
 
 local PX = 30
 
 local game = State({
   entities = {},
+  aabbs = {},
   player = nil,
   viewWidth = 640,
   viewHeight = 448,
@@ -26,7 +27,7 @@ function doWrap(e, st)
   elseif e.pos.y > st.height - PX then e.pos.y = e.pos.y - st.height
   end
 
-  LOG.info("wrap " .. e.name)
+  --LOG.info("wrap " .. e.name)
 end
 
 function game:enter()
@@ -38,6 +39,12 @@ end
 
 function game:spawn(e)
   table.insert(self.entities, e)
+  LOG.debug("spawn " .. e.name .. " @ " .. tostring(e.pos))
+  e:onSpawn(self)
+end
+
+function game:addArea(b)
+  table.insert(self.aabbs, b)
 end
 
 function game:draw()
@@ -54,6 +61,7 @@ function game:update(dt)
   for i, e in ipairs(self.entities) do
     if e:update(dt, self) == false then
       table.insert(killList, i)
+      e:onDestroy(self)
     end
     if e.pos.x < -PX or e.pos.y < -PX or 
       e.pos.x > self.width or e.pos.y > self.height then
@@ -65,8 +73,30 @@ function game:update(dt)
     end
   end
 
+  local aabbKillList = {}
+  for i, aabb in ipairs(self.aabbs) do
+    if aabb.active == false then
+      table.insert(aabbKillList, i)
+    else
+      for _, e in ipairs(self.entities) do
+        e:collide(aabb)
+      end
+    end
+  end
+
   for _, i in ipairs(killList) do
     table.remove(self.entities, i)
+    for j=i+1,#killList,1 do
+      if killList[j] > killList[i]  then killList[i] = killList[i] - 1 end
+    end
+  end
+
+  for _, i in ipairs(aabbKillList) do
+    table.remove(self.aabbs, i)
+    for j=i+1,#aabbKillList,1 do
+      if aabbKillList[j] > aabbKillList[i]  then aabbKillList[i] = aabbKillList[i] - 1 end
+    end
+
   end
 end
 
